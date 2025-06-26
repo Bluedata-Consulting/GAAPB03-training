4a039d345703663ced405a28b5d096013d84d866
Anshu Pandey <anshuspandey@gmail.com>
2025-06-26 21:30:26 +0530

    modified code for MCP

--- mcpcode/mcpserver3.py
+++ mcpcode/mcpserver3.py
@@ -5,6 +5,7 @@ import requests, json
 
 mcp = FastMCP("myMCP")
 
+
 @mcp.tool()
 async def get_current_weather(city:str)->dict:
     """ can be used to get/fetch current weather information for a city name
@@ -27,7 +28,8 @@ async def get_wikipedia_pages(query:str)->list:
     info = wikipedia.search(query)
     return info
 
+import uvicorn
 
 if __name__=="__main__":
-    mcp.run(transport="http")
+    uvicorn.run(httpapp,host="0.0.0.0",port=8000)
 
